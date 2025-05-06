2f143c17f9c37ad61ca6719054c6a8f012f72a05
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-05-06 09:38:42 +0200

    chore: Add http module to imports
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [EmailService],
   exports: [EmailService]
 })
