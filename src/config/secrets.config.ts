eb53b2db19542f35e0eb0aa545e87f0e8891b6d7
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-05-06 09:38:07 +0200

    chore: Add new env secret
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/config/secrets.config.ts
+++ src/config/secrets.config.ts
@@ -4,4 +4,5 @@ export default registerAs('secrets', () => ({
   jwt: process.env.JWT_SECRET,
   cookie: process.env.COOKIE_SECRET,
   password: process.env.PASSWORD_SECRET,
+  site_url: process.env.SITE_URL
 }));
