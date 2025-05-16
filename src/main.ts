a42789e697b60944902a3e2d45d36d6fd5b7a09d
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-05-17 00:20:43 +0200

    hotfixes

--- src/main.ts
+++ src/main.ts
@@ -75,8 +75,8 @@ async function bootstrap() {
   );
 
   await app.listen(
-    process.env.PORT ?? 3000,
-    process.env.API_BIND_IP ?? '127.0.0.1',
+    process.env.PORT ?? 3080,
+    // process.env.API_BIND_IP ?? '127.0.0.1',
   );
 
   if (module.hot) {
