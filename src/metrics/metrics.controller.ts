9628e00064cdc9d4285b196c9252eeb8c3125744
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-05-16 23:51:36 +0200

    fix: Allow metrics endpoint to be public

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import * as client from 'prom-client';
+import { Public } from 'src/auth/auth.guard';
+import { AllowSuspended } from 'src/users/suspension.guard';
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics();
@@ -7,6 +9,8 @@ collectDefaultMetrics();
 @Controller('metrics')
 export class MetricsController {
   @Get()
+  @AllowSuspended()
+  @Public()
   getMetrics(): Promise<string> {
     return client.register.metrics();
   }
